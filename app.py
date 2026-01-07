e74abd5009979d9ac679ffbbe2dc5392549ce331
HandleyECTS <handleylogan01@gmail.com>
2026-01-07 12:49:40 -0500

    forced a merge on the local side

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 print("Git(Hub) Demo")
-print("hello from the local repo!")
-print("hello from the remote repo")
+print("hello from the local repo! 0")
+print("hello from the remote repo 1")
+print("hey remote. yous a dummy")
