238beea41dec50b03c9d57a6d01ab40df3a8125a
HandleyECTS <144816677+HandleyECTS@users.noreply.github.com>
2026-01-07 12:49:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 print("Git(Hub) Demo")
-print("hello from the local repo!")
-print("hello from the remote repo")
+print("hello from the local repo! 2")
+print("hello from the remote repo 3")
+print("hey local. yous ain't so tuff")
